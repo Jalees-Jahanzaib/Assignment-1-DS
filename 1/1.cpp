31b89b7cf71940c9c843dfc59d84d3c089c2ce1f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2021-01-16 22:49:01 +0530

    Q1 answer incorrect

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,22 +5,20 @@
 #include "mpi.h"
 #include <fstream>
 #include <bits/stdc++.h>
+#define Big 1000000
 using namespace std;
+
 typedef long long int ll;
 int main( int argc, char **argv ) {
-    int rank, numprocs;
+    int rank, size;
 
-    /* start up MPI */
+
     MPI_Init( &argc, &argv );
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
-    
-    /*synchronize all processes*/
-    MPI_Barrier( MPI_COMM_WORLD );
-    double tbeg = MPI_Wtime();
-int size=numprocs;
-    /* write your code here */
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+
 if(rank==0){
     int N;
     cin>>N;
@@ -28,35 +26,28 @@ if(rank==0){
     MPI_Send(&N,1,MPI_INT,i,0,MPI_COMM_WORLD);
 
     }
-		double sum=0;
+		ll sum=0;
 
 		for(int i=1;i<size;i++){
 
-			double temp;
-			MPI_Recv(&temp, 1, MPI_DOUBLE,i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			ll temp;
+			MPI_Recv(&temp, 1, MPI_LONG,i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			sum+=temp;
 		}
-		for(int i=1;i<N/size;i++){sum+=pow(i,-2);}
-		cout<<sum<<endl;
+		for(int i=1;i<N/size;i++){sum+=pow(i,-2)*Big;}
+        int arr1=(sum%Big);
+		cout<<sum/Big<<"."<<arr1<<endl;
 	}
 	else{
         int N;
         MPI_Recv(&N, 1, MPI_INT,0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 		int start=rank*(N/size);
-		double sum=0;
-		for(int i=start;i<=start+N/size;i++){sum+=pow(i,-2);}
-		MPI_Send(&sum,1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		ll sum=0;
+		for(int i=start;i<=start+N/size;i++){sum+=pow(i,-2)*Big;}
+		MPI_Send(&sum,1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
 	}
-    MPI_Barrier( MPI_COMM_WORLD );
-    double elapsedTime = MPI_Wtime() - tbeg;
-    double maxTime;
-    MPI_Reduce( &elapsedTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD );
-    if ( rank == 0 ) {
-        printf( "Total time (s): %f\n", maxTime );
-    }
 
-    /* shut down MPI */
     MPI_Finalize();
     return 0;
 }
