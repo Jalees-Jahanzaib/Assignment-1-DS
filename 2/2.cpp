0b4d90fd8211cd1b5b99f8ee3addeae8555cce05
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2021-01-20 21:10:44 +0530

    Printing true values but has issues with Extra processes

--- 2/2.cpp
+++ 2/2.cpp
@@ -37,53 +37,51 @@ void quicksort(int *A, int lo, int hi) {
 		quicksort(A, p + 1, hi);
 	}
 }
-int get_pivot(int *arr, int low, int high)
-{
-    int pivot = arr[high];   
-    int i = (low - 1);  
- 
-    for (int j = low; j <= high- 1; j++)
-    {
-
-        if (arr[j] <= pivot)
-        {
-            i++;    
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
+int get_pivot(int *A, int lo, int hi) {
+	int pivot = A[hi];
+	int i = lo - 1;
+	for (int j = lo; j < hi; j++) {
+		if (A[j] <= pivot) {
+			i++;
+			int temp = A[i];
+			A[i] = A[j];
+			A[j] = temp;
+		}
+	}
+	int temp = A[i + 1];
+	A[i + 1] = A[hi];
+	A[hi] = temp;
+	return i + 1;
 }
 void ParallelSort( int* arr,int arr_size, int my_rank, int Level, int Max_Possible_Rank){
     int Sharing_process_rank= my_rank+ pow(2,Level);
     Level++;
-        cout<<"Check"<<" "<<" 1"<<endl;
-
+    MPI_Status status;
+    if(my_rank >= arr_size|| Level>=arr_size)
+    return ;
     if(Sharing_process_rank>Max_Possible_Rank){
         //perfrom Sequential Sort
 quicksort(arr,0,arr_size-1);
         return;
     }
-    int Pivot_Index=get_pivot(arr,0,arr_size-1);
-    int start=0; 
-    while(Pivot_Index==start-1){
-        Pivot_Index=get_pivot(arr,start,arr_size-1);
-        start++;
-    }
-    cout<<"Pivot "<<Pivot_Index<<endl;
-    if(Pivot_Index<=arr_size-Pivot_Index){
-        MPI_Send(&Pivot_Index,1,MPI_INT,Sharing_process_rank,Pivot_Index,MPI_COMM_WORLD);
-        MPI_Send(arr,(Pivot_Index-1),MPI_INT,Sharing_process_rank,Pivot_Index,MPI_COMM_WORLD);
-        ParallelSort(arr,(arr_size-Pivot_Index+1),my_rank,Level,Max_Possible_Rank);
-        MPI_Recv(&arr,(Pivot_Index-1),MPI_INT,Sharing_process_rank,MPI_ANY_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+   int j = 0;
+	int pivotIndex;
+	do {
+		pivotIndex = get_pivot(arr, j, arr_size - 1);
+		j++;
+	} while (pivotIndex == j - 1);
+    cout<<"Pivot "<<pivotIndex<<endl;
+    if(pivotIndex<=arr_size-pivotIndex){
+        MPI_Send(arr,(pivotIndex-1),MPI_INT,Sharing_process_rank,pivotIndex,MPI_COMM_WORLD);
+        ParallelSort((arr+pivotIndex+1),(arr_size-pivotIndex-1),my_rank,Level,Max_Possible_Rank);
+        MPI_Recv(arr,(pivotIndex-1),MPI_INT,Sharing_process_rank,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 
     }
     else{
-        int K=(arr_size-Pivot_Index-1);
-        MPI_Send(&K,1,MPI_INT,Sharing_process_rank,Pivot_Index,MPI_COMM_WORLD);
-        MPI_Send(arr,K,MPI_INT,Sharing_process_rank,Pivot_Index,MPI_COMM_WORLD);
-        ParallelSort(arr,K,my_rank,Level,Max_Possible_Rank);
-        MPI_Recv(arr,(Pivot_Index-1),MPI_INT,Sharing_process_rank,MPI_ANY_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        int K=(arr_size-pivotIndex-1);
+        MPI_Send(arr+pivotIndex+1,K,MPI_INT,Sharing_process_rank,pivotIndex+1,MPI_COMM_WORLD);
+        ParallelSort(arr,(pivotIndex+1),my_rank,Level,Max_Possible_Rank);
+        MPI_Recv((arr+pivotIndex+1),(arr_size-pivotIndex-1),MPI_INT,Sharing_process_rank,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 
     }
     
@@ -100,6 +98,7 @@ int main( int argc, char **argv ) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
     // Calculate Height and of the tree
+    
     int Level=0;
     int RankPower=pow(2,Level);
     while(RankPower<=rank){
@@ -109,11 +108,12 @@ int main( int argc, char **argv ) {
 	if(rank==0){
 int N;
 cin>>N;
-int arr[N];
+int *arr = (int*)malloc(N * sizeof(int));
     for(int i=0;i<N;i++)
     {
         cin>>arr[i];
     }
+
     ParallelSort(arr,N,rank,Level,size-1);
         for(int i=0;i<N;i++)
     {
@@ -123,17 +123,24 @@ int arr[N];
     }
 
     else{
-        int subarrIndex;
-        MPI_Recv(&subarrIndex,1,MPI_INT,rank,Level,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        int subarray[subarrIndex];
+
         MPI_Status status;
-        MPI_Recv(subarray,subarrIndex,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-        ParallelSort(subarray,subarrIndex,rank,Level,size-1);
-        MPI_Send(subarray,subarrIndex,MPI_INT,status.MPI_SOURCE,0,MPI_COMM_WORLD);
+		int subarray_size;
+		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, MPI_INT, &subarray_size);
+        // int subarray[subarray_size];
+        // MPI_Recv(subarray,subarray_size,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+        // ParallelSort(subarray,subarray_size,rank,Level,size-1);
+        // MPI_Send(subarray,subarray_size,MPI_INT,status.MPI_SOURCE,0,MPI_COMM_WORLD);
         
+         int source_process = status.MPI_SOURCE;
+			int *subarray = (int*)malloc(subarray_size * sizeof(int));
 
+			MPI_Recv(subarray, subarray_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                ParallelSort(subarray,subarray_size,rank,Level,size-1);
+			MPI_Send(subarray, subarray_size, MPI_INT, source_process, 0, MPI_COMM_WORLD);
 
-    }
+     }
     MPI_Finalize(); 
     return 0;
 }
